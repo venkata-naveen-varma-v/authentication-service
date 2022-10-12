e332fffc2cc1569a51f2b3eeb23bebd4a7977ae3
“venkata-naveen-varma” <venkata2naveen@gmail.com>
2022-10-12 15:04:30 -0400

    user registration

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const userController = require('../controllers/user')
-const middleware = require('../middleware/auth')
+// const middleware = require('../middleware/auth')
 
 const router = express.Router()
 
