9c5a6c925b24808e333fdb2546d16a003e6e9fe3
“venkata-naveen-varma” <venkata2naveen@gmail.com>
2022-10-12 15:22:53 -0400

    other feature

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const userController = require('../controllers/user')
-// const middleware = require('../middleware/auth')
+const middleware = require('../middleware/auth')
 
 const router = express.Router()
 
 router.post('/register', userController.register)
+router.post('/login', userController.login)
+router.post('/logout', middleware.auth, userController.logout)
+router.post('/details', middleware.auth, userController.userdetails)
 
 module.exports = router;
